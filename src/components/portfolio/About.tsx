d7c32370ede638e08bd869aaf064a18b90a5b820
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2026-01-06 17:33:37 +0300

    impoved the hero

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
   const highlights = [
     {
       icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <svg className="w-9 h-9" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
         </svg>
       ),
